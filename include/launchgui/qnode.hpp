dfe62fb1c86aa32a978bb4a8bfa446985d6a6348
Mintuur <mintuur0525@gmail.com>
2021-06-24 22:05:10 +0900

    first commit 2021-06-24

---
+++ include/launchgui/qnode.hpp
@@ -0,0 +1,82 @@
+/**
+ * @file /include/launchgui/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef launchgui_QNODE_HPP_
+#define launchgui_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+// To workaround boost/qt4 problems that won't be bugfixed. Refer to
+//    https://bugreports.qt.io/browse/QTBUG-22829
+#ifndef Q_MOC_RUN
+#include <ros/ros.h>
+#endif
+#include <string>
+#include <QThread>
+#include <QStringListModel>
+
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace launchgui {
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+
+class QNode : public QThread {
+    Q_OBJECT
+public:
+	QNode(int argc, char** argv );
+	virtual ~QNode();
+	bool init();
+	bool init(const std::string &master_url, const std::string &host_url);
+	void run();
+
+	/*********************
+	** Logging
+	**********************/
+	enum LogLevel {
+	         Debug,
+	         Info,
+	         Warn,
+	         Error,
+	         Fatal
+	 };
+
+	QStringListModel* loggingModel() { return &logging_model; }
+	void log( const LogLevel &level, const std::string &msg);
+
+Q_SIGNALS:
+	void loggingUpdated();
+    void rosShutdown();
+    void AutoDriving();
+    void Door();
+    void Obstacle();
+    void Parking();
+    void Stair();
+    void Start();
+    void All_stop();
+
+private:
+	int init_argc;
+	char** init_argv;
+        ros::Publisher mission_publisher;
+    QStringListModel logging_model;
+};
+
+}  // namespace launchgui
+
+#endif /* launchgui_QNODE_HPP_ */
